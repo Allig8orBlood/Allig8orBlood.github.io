c84837b5f4ae2cefdc9b252deefacbacc72f5f53
Allig8orBlood <Allig8orBlood182@yandex.com>
2019-06-21 12:57:41 +0900

    commit test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(function() {
+
+   var scrollMenu = $('.scrollMenu');
+   scrollMenu.hide();
+
+   $(window).scroll(function() {
+      if($(this).scrollTop() > 80) {
+         scrollMenu.fadeIn();
+      } else {
+         scrollMenu.fadeOut();
+      }
+   });
+
    $('#toggleMenu').css('display','none');
    $('#navToggle').on('click', function() {
       $('#toggleMenu').slideToggle(200);
       $(this).toggleClass('active');
    });
+
 });
